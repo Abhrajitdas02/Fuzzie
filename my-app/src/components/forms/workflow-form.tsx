5d303038545454a0ac4bec0900207cb3195e7619
IndranujGhosh <debasreeghosh78@gmail.com>
2024-07-04 03:50:53 +0530

    workflow updates

--- my-app/src/components/forms/workflow-form.tsx
+++ my-app/src/components/forms/workflow-form.tsx
@@ -3,7 +3,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/navigation'
 import React from 'react'
 import { useForm } from 'react-hook-form'
-import { z } from 'zod'
+import { z } from 'zod' 
 import {
   Card,
   CardContent,
